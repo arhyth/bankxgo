85dee6cbd0e0e90470f110a00e48f56b2996b1ec
David Ebreo <arhyth@gmail.com>
2024-09-16 18:24:36 +0800

    Implement handlers for Deposit, Withdraw

---
+++ http.go
@@ -0,0 +1,211 @@
+package bankxgo
+
+import (
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+
+	"github.com/bwmarrin/snowflake"
+	"github.com/go-chi/chi/v5"
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
+	"github.com/shopspring/decimal"
+)
+
+var (
+	statusOK = []byte(`{"status":"OK"}`)
+)
+
+func NewHTTPHandler(svc Service, log *zerolog.Logger) http.Handler {
+	hndlr := &httpHandler{
+		Svc: svc,
+		Log: log,
+	}
+	mux := chi.NewMux()
+	mux.NotFound(HTTPNotFound)
+	mux.Route("/accounts", func(r chi.Router) {
+		r.Route("/{acctID:[0-9]+}", func(rr chi.Router) {
+			rr.Post("/deposit", hndlr.Deposit)
+			rr.Post("/withdraw", hndlr.Withdraw)
+			rr.Get("/balance", hndlr.Balance)
+			rr.Get("/statement", hndlr.Statement)
+		})
+	})
+
+	return mux
+}
+
+type httpHandler struct {
+	Svc Service
+	Log *zerolog.Logger
+}
+
+func (h *httpHandler) Deposit(w http.ResponseWriter, r *http.Request) {
+	buf, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		h.Log.Err(err).Str("method", "deposit").Msg("error reading HTTP request")
+		WriteHTTPError(w, ErrInternalServer)
+		return
+	}
+	var req ChargeReq
+	if err = json.Unmarshal(buf, &req); err != nil {
+		h.Log.Err(err).Str("method", "deposit").Msg("error unmarshalling JSON")
+		WriteHTTPError(w, ErrBadRequest{Fields: map[string]string{"request body": "malformed JSON"}})
+		return
+	}
+	pid := chi.URLParam(r, "acctID")
+	acctID, err := snowflake.ParseString(pid)
+	if err != nil {
+		h.Log.Err(err).Str("method", "deposit").Msg("error parsing account ID")
+		WriteHTTPError(w, ErrBadRequest{map[string]string{"acctID": "invalid format"}})
+		return
+	}
+	req.AcctID = acctID
+	if err = h.Svc.Deposit(req); err != nil {
+		WriteHTTPError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(statusOK)
+	if err != nil {
+		WriteHTTPError(w, err)
+	}
+}
+
+func (h *httpHandler) Withdraw(w http.ResponseWriter, r *http.Request) {
+	buf, err := io.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		h.Log.Err(err).Str("method", "withdraw").Msg("error reading HTTP request")
+		WriteHTTPError(w, ErrInternalServer)
+		return
+	}
+	var req ChargeReq
+	if err = json.Unmarshal(buf, &req); err != nil {
+		h.Log.Err(err).Str("method", "withdraw").Msg("error unmarshalling JSON")
+		WriteHTTPError(w, ErrBadRequest{Fields: map[string]string{"request body": "malformed JSON"}})
+		return
+	}
+	pid := chi.URLParam(r, "acctID")
+	acctID, err := snowflake.ParseString(pid)
+	if err != nil {
+		h.Log.Err(err).Str("method", "withdraw").Msg("error parsing account ID")
+		WriteHTTPError(w, ErrBadRequest{map[string]string{"acctID": "invalid format"}})
+		return
+	}
+	req.AcctID = acctID
+	if err = h.Svc.Withdraw(req); err != nil {
+		WriteHTTPError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	_, err = w.Write(statusOK)
+	if err != nil {
+		WriteHTTPError(w, err)
+	}
+}
+
+func (h *httpHandler) Balance(w http.ResponseWriter, r *http.Request) {
+	email := r.Header.Get("email")
+	if email == "" {
+		h.Log.Error().Str("method", "balance").Msg("missing/invalid email")
+		WriteHTTPError(w, ErrBadRequest{map[string]string{"email": "missing or invalid"}})
+		return
+	}
+
+	pid := chi.URLParam(r, "acctID")
+	acctID, err := snowflake.ParseString(pid)
+	if err != nil {
+		h.Log.Err(err).Str("method", "balance").Msg("error parsing account ID")
+		WriteHTTPError(w, ErrBadRequest{map[string]string{"acctID": "invalid format"}})
+		return
+	}
+	req := BalanceReq{
+		AcctID: acctID,
+		Email:  email,
+	}
+	bal, err := h.Svc.Balance(req)
+	if err != nil {
+		WriteHTTPError(w, err)
+		return
+	}
+
+	resp := map[string]decimal.Decimal{
+		"balance": bal,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err = json.NewEncoder(w).Encode(resp); err != nil {
+		WriteHTTPError(w, err)
+	}
+}
+
+func (h *httpHandler) Statement(w http.ResponseWriter, r *http.Request) {
+	email := r.Header.Get("email")
+	if email == "" {
+		h.Log.Error().Str("method", "statement").Msg("missing/invalid email")
+		WriteHTTPError(w, ErrBadRequest{map[string]string{"email": "missing or invalid"}})
+		return
+	}
+	pid := chi.URLParam(r, "acctID")
+	acctID, err := snowflake.ParseString(pid)
+	if err != nil {
+		h.Log.Err(err).Str("method", "statement").Msg("error parsing account ID")
+		WriteHTTPError(w, ErrBadRequest{map[string]string{"acctID": "invalid format"}})
+		return
+	}
+	req := StatementReq{
+		AcctID: acctID,
+		Email:  email,
+	}
+	if err = h.Svc.Statement(w, req); err != nil {
+		WriteHTTPError(w, err)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/pdf")
+	_, err = w.Write(statusOK)
+	if err != nil {
+		WriteHTTPError(w, err)
+	}
+}
+
+func WriteHTTPError(w http.ResponseWriter, err error) {
+	var ne error
+	defer func() {
+		if ne != nil {
+			log.Error().
+				Err(ne).
+				Msg("error response encoding failed")
+		}
+	}()
+
+	w.Header().Set("Content-Type", "application/json")
+	errnf := &ErrNotFound{}
+	errbr := &ErrBadRequest{}
+	if errors.As(err, errnf) {
+		w.WriteHeader(http.StatusNotFound)
+		ne = json.NewEncoder(w).Encode(errnf)
+	} else if errors.As(err, errbr) {
+		w.WriteHeader(http.StatusBadRequest)
+		ne = json.NewEncoder(w).Encode(errbr)
+	} else {
+		w.WriteHeader(http.StatusInternalServerError)
+		resp := map[string]string{
+			"message": "server error",
+		}
+		ne = json.NewEncoder(w).Encode(resp)
+	}
+}
+
+func HTTPNotFound(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	w.Header().Set("Content-Type", "application/json")
+	resp := map[string]string{
+		"path": r.URL.Path,
+	}
+	json.NewEncoder(w).Encode(resp)
+}
---
+++ http_test.go
@@ -0,0 +1,180 @@
+package bankxgo_test
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/rs/zerolog"
+	"github.com/shopspring/decimal"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.uber.org/mock/gomock"
+
+	"github.com/arhyth/bankxgo"
+	"github.com/arhyth/bankxgo/mocks"
+)
+
+func TestHTTPDeposit(t *testing.T) {
+	nooplog := zerolog.Nop()
+
+	t.Run("Deposit returns OK on success", func(tt *testing.T) {
+		as := assert.New(tt)
+		ctrl := gomock.NewController(tt)
+		svc := mocks.NewMockService(ctrl)
+		svc.EXPECT().
+			Deposit(gomock.AssignableToTypeOf(bankxgo.ChargeReq{})).
+			DoAndReturn(func(r bankxgo.ChargeReq) error {
+				return nil
+			}).
+			Times(1)
+
+		hndlr := bankxgo.NewHTTPHandler(svc, &nooplog)
+		body := bytes.NewBufferString(`{"amount":1234.00}`)
+		req := httptest.NewRequest(http.MethodPost, "/accounts/1834563581361305763/deposit", body)
+		req.Header.Set("email", "arhyth@gmail.com")
+		w := httptest.NewRecorder()
+		hndlr.ServeHTTP(w, req)
+
+		resp := map[string]string{}
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+		as.Nil(err)
+		as.Contains(resp, "status")
+		as.Equal(resp["status"], "OK")
+	})
+
+	t.Run("/accounts/{acctID}/deposit returns error on invalid account ID", func(tt *testing.T) {
+		as := assert.New(tt)
+		reqrd := require.New(tt)
+		ctrl := gomock.NewController(t)
+		svc := mocks.NewMockService(ctrl)
+		hndlr := bankxgo.NewHTTPHandler(svc, &nooplog)
+
+		body := bytes.NewBufferString(`{"amount":1234.00}`)
+		req := httptest.NewRequest(http.MethodPost, "/accounts/24j24g*()/deposit", body)
+		req.Header.Set("email", "rogue@one.com")
+		w := httptest.NewRecorder()
+		hndlr.ServeHTTP(w, req)
+
+		resp := map[string]string{}
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+		reqrd.Nil(err)
+		as.Contains(resp, "path")
+	})
+
+	t.Run("/accounts/{acctID}/deposit returns error on malformed request body", func(tt *testing.T) {
+		as := assert.New(tt)
+		reqrd := require.New(tt)
+		ctrl := gomock.NewController(t)
+		svc := mocks.NewMockService(ctrl)
+		hndlr := bankxgo.NewHTTPHandler(svc, &nooplog)
+
+		body := bytes.NewBufferString(`{"amount":1234.00`)
+		req := httptest.NewRequest(http.MethodPost, "/accounts/123456789/deposit", body)
+		req.Header.Set("email", "rogue@one.com")
+		w := httptest.NewRecorder()
+		hndlr.ServeHTTP(w, req)
+
+		resp := map[string]map[string]string{}
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+		reqrd.Nil(err)
+		as.Contains(resp, "fields")
+		as.Contains(resp["fields"], "request body")
+	})
+}
+func TestHTTPWithdraw(t *testing.T) {
+	nooplog := zerolog.Nop()
+	t.Run("Withdraw returns OK on success", func(tt *testing.T) {
+		as := assert.New(tt)
+		ctrl := gomock.NewController(tt)
+		svc := mocks.NewMockService(ctrl)
+		svc.EXPECT().
+			Withdraw(gomock.AssignableToTypeOf(bankxgo.ChargeReq{})).
+			DoAndReturn(func(r bankxgo.ChargeReq) error {
+				return nil
+			}).
+			Times(1)
+
+		hndlr := bankxgo.NewHTTPHandler(svc, &nooplog)
+		body := bytes.NewBufferString(`{"amount":1234.00}`)
+		req := httptest.NewRequest(http.MethodPost, "/accounts/1834563581361305763/withdraw", body)
+		req.Header.Set("email", "arhyth@gmail.com")
+		w := httptest.NewRecorder()
+		hndlr.ServeHTTP(w, req)
+
+		resp := map[string]string{}
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+		as.Nil(err)
+		as.Contains(resp, "status")
+		as.Equal(resp["status"], "OK")
+	})
+
+	t.Run("/accounts/{acctID}/withdraw returns error on invalid account ID", func(tt *testing.T) {
+		as := assert.New(tt)
+		reqrd := require.New(tt)
+		ctrl := gomock.NewController(tt)
+		svc := mocks.NewMockService(ctrl)
+		hndlr := bankxgo.NewHTTPHandler(svc, &nooplog)
+
+		body := bytes.NewBufferString(`{"amount":1234.00}`)
+		req := httptest.NewRequest(http.MethodPost, "/accounts/24j24g*()/withdraw", body)
+		req.Header.Set("email", "rogue@one.com")
+		w := httptest.NewRecorder()
+		hndlr.ServeHTTP(w, req)
+
+		resp := map[string]string{}
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+		reqrd.Nil(err)
+		as.Contains(resp, "path")
+	})
+
+	t.Run("/accounts/{acctID}/withdraw returns error on malformed request body", func(tt *testing.T) {
+		as := assert.New(tt)
+		reqrd := require.New(tt)
+		ctrl := gomock.NewController(t)
+		svc := mocks.NewMockService(ctrl)
+		hndlr := bankxgo.NewHTTPHandler(svc, &nooplog)
+
+		body := bytes.NewBufferString(`{"amount":1234.00`)
+		req := httptest.NewRequest(http.MethodPost, "/accounts/123456789/withdraw", body)
+		req.Header.Set("email", "rogue@one.com")
+		w := httptest.NewRecorder()
+		hndlr.ServeHTTP(w, req)
+
+		resp := map[string]map[string]string{}
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+		reqrd.Nil(err)
+		as.Contains(resp, "fields")
+		as.Contains(resp["fields"], "request body")
+	})
+}
+
+func TestHTTPBalance(t *testing.T) {
+	nooplog := zerolog.Nop()
+	t.Run("Balance returns balance amount", func(tt *testing.T) {
+		as := assert.New(tt)
+		ctrl := gomock.NewController(tt)
+		svc := mocks.NewMockService(ctrl)
+		balance := decimal.NewFromFloat(123.45)
+		svc.EXPECT().
+			Balance(gomock.AssignableToTypeOf(bankxgo.BalanceReq{})).
+			DoAndReturn(func(r bankxgo.BalanceReq) (decimal.Decimal, error) {
+				return balance, nil
+			}).
+			Times(1)
+
+		hndlr := bankxgo.NewHTTPHandler(svc, &nooplog)
+		req := httptest.NewRequest(http.MethodGet, "/accounts/1834563581361305763/balance", nil)
+		req.Header.Set("email", "arhyth@gmail.com")
+		w := httptest.NewRecorder()
+		hndlr.ServeHTTP(w, req)
+
+		resp := map[string]string{}
+		err := json.Unmarshal(w.Body.Bytes(), &resp)
+		as.Nil(err)
+		as.Contains(resp, "balance")
+		as.Equal(resp["balance"], balance.String())
+	})
+}
